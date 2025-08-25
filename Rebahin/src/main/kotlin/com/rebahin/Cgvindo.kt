66154421ba46bbe90dd6bf3fbdac0e6fb9d2aa94
santveraaa <santveraaa@gmail.com>
2025-08-25 13:35:19 +0700

    delete

--- Rebahin/src/main/kotlin/com/rebahin/Cgvindo.kt
+++
@@ -1,8 +0,0 @@
-package com.rebahin
-
-class Cgvindo : Rebahin() {
-
-    // override var mainUrl = "http://62.171.170.1/"
-    override var mainUrl = "https://cgvindo.christmas/"
-    override var name = "Cgvindo"
-}
